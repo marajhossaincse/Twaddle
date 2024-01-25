2610220aa84cc2400086c795b323b4423a1414d3
Maraj Hossain <marajhossaincse@gmail.com>
2024-01-25 15:04:24 +0600

    Code refactoring.

--- Twaddle/View/UserDetailScreen.swift
+++ Twaddle/View/UserDetailScreen.swift
@@ -9,32 +9,20 @@ import SwiftUI
 
 struct UserDetailScreen: View {
     var user: User
-    
+
     var body: some View {
         Form {
             Section {
                 Text("Name: \(user.getName())")
-                
+
                 Text("Email: \(user.getEmail())")
-                
+
                 Text("Sex: \(user.getGender())")
-                
+
             } header: {
                 Text("Baic Info:")
             }
         }
-        
-//        VStack(spacing: 0) {
-//            Image(systemName: "exclamationmark.octagon")
-//                .resizable()
-//                .renderingMode(.original)
-//                .aspectRatio(1, contentMode: .fit)
-//                .frame(width: 50, height: 50)
-//                .padding()
-//
-//            Text("Under Construction")
-//                .font(.title)
-//        }
     }
 }
 
