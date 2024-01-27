cd3d5d0176befb5f17b2aec3622e4999f8bb6173
Maraj Hossain <marajhossaincse@gmail.com>
2024-01-27 18:28:01 +0600

    Remove post model and viewmodel.

--- Twaddle/ViewModel/UserViewModel.swift
+++ Twaddle/ViewModel/UserViewModel.swift
@@ -10,6 +10,38 @@ import Foundation
 class UserViewModel: ObservableObject {
     @Published var users: [User] = []
 
+//    func fetchUser() {
+//        // Check if URL is correct
+//        guard let url = URL(string: "https://gorest.co.in/public/v2/users") else {
+//            print("Invalid URL")
+//            return
+//        }
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            if let error = error {
+//                print("Error here of: \(error.localizedDescription)")
+//                return
+//            }
+//
+//            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+//                print("Invalid response")
+//                return
+//            }
+//
+//            if let data = data {
+//                do {
+//                    let decodedData = try JSONDecoder().decode([User].self, from: data)
+//                    DispatchQueue.main.async {
+//                        self.users = decodedData
+//                    }
+//                } catch {
+//                    print("Error decoding JSON: \(error)")
+//                }
+//            }
+//        }
+//        .resume()
+//    }
+
     func fetchUser() {
         // take  url
         guard let url = URL(string: "https://gorest.co.in/public/v2/users") else {
