e1015ee60ff094bd34fe2628d48bb0b2ce473188
Maraj Hossain <marajhossaincse@gmail.com>
2024-01-28 15:18:46 +0600

    Add user adding screen sheet.

---
+++ Twaddle/View/Components/AddUserSheet.swift
@@ -0,0 +1,20 @@
+//
+//  AddUserSheet.swift
+//  Twaddle
+//
+//  Created by Maraj Hossain on 1/28/24.
+//
+
+import SwiftUI
+
+struct AddUserSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddUserSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        AddUserSheet()
+    }
+}
