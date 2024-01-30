f096485bed84522c15e58074888a5b56628e2b33
Maraj Hossain <marajhossaincse@gmail.com>
2024-01-30 12:36:49 +0600

    Add symbols.

---
+++ Twaddle/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  Twaddle
+//
+//  Created by Maraj Hossain on 1/30/24.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
