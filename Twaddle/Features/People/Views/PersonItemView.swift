2604ff18e499f0ad81d7ce6b58dd8d82c3d41bdc
Maraj Hossain <marajhossaincse@gmail.com>
2024-02-15 11:57:42 +0600

    Add image to people view.

--- Twaddle/Features/People/Views/PersonItemView.swift
+++ Twaddle/Features/People/Views/PersonItemView.swift
@@ -12,9 +12,15 @@ struct PersonItemView: View {
 
     var body: some View {
         VStack(spacing: 0) {
-            Rectangle()
-                .fill(.blue)
-                .frame(height: 130)
+            AsyncImage(url: .init(string: user.avatar)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 130)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
+            }
 
             VStack(alignment: .leading) {
                 PillView(id: user.id)
