d30865129b5265fa12df96880377ccda8d34e31b
Maraj Hossain <marajhossaincse@gmail.com>
2024-02-20 16:47:03 +0600

    Add singleton for networking manager.

---
+++ Twaddle/Base/API/NetworkingManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingManager.swift
+//  Twaddle
+//
+//  Created by Maraj Hossain on 2/20/24.
+//
+
+import Foundation
+
+final class NetworkingManager {
+    static let shared = NetworkingManager()
+
+    private init() {}
+}
