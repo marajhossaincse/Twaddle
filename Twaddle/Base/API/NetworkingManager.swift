140e61539c6667304354e29d716042acd72bbb66
Maraj Hossain <marajhossaincse@gmail.com>
2024-02-21 12:32:43 +0600

    Add request function and make it generic.

--- Twaddle/Base/API/NetworkingManager.swift
+++ Twaddle/Base/API/NetworkingManager.swift
@@ -11,4 +11,36 @@ final class NetworkingManager {
     static let shared = NetworkingManager()
 
     private init() {}
+
+    func request<T: Codable>(_ absoluteURL: String, type: T.Type) {
+        let url = URL(string: absoluteURL)
+
+        let request = URLRequest(url: url!)
+
+        let dataTasks = URLSession.shared.dataTask(with: request) { data, response, error in
+
+            if error != nil {
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse,
+                  (200 ... 300) ~= response.statusCode
+            else {
+                return
+            }
+
+            guard let data = data else {
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let result = try decoder.decode(T.self, from: data)
+            } catch {
+                print(error)
+            }
+        }
+        dataTasks.resume()
+    }
 }
