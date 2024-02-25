b99a7be6893e0a68f10d6ee21a07f435b798dce2
Maraj Hossain <marajhossaincse@gmail.com>
2024-02-25 16:11:32 +0600

    Fix data changes publishing from background thread to main thread.

--- Twaddle/Features/People/ViewModels/PeopleViewModel.swift
+++ Twaddle/Features/People/ViewModels/PeopleViewModel.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 
-final class PeopleViewModel: ObservableObject{
-
+final class PeopleViewModel: ObservableObject {
     @Published private(set) var users: [User] = []
     
-    func fetchUsers(){
+    func fetchUsers() {
         NetworkingManager.shared.request(
             "https://reqres.in/api/users/1",
             type: UsersReponse.self
         ) { [weak self] res in
-            switch res {
-            case .success(let resultData):
-                self?.users = resultData.data
-            case .failure(let error):
-                print(error)
+           
+            DispatchQueue.main.async {
+                switch res {
+                case .success(let resultData):
+                    self?.users = resultData.data
+                case .failure(let error):
+                    print(error)
+                }
             }
         }
     }
