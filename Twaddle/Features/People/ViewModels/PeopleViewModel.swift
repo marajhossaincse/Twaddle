9072c47114f758ff27e99aa9132ec0e6b32480f8
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-03 17:42:12 +0600

    Add properties to handle networking errors.

--- Twaddle/Features/People/ViewModels/PeopleViewModel.swift
+++ Twaddle/Features/People/ViewModels/PeopleViewModel.swift
@@ -9,13 +9,15 @@ import Foundation
 
 final class PeopleViewModel: ObservableObject {
     @Published private(set) var users: [User] = []
-    
+    @Published private(set) var error: NetworkingManager.NetworkingError?
+    @Published var hasError = false
+
     func fetchUsers() {
         NetworkingManager.shared.request(
             "https://reqres.in/api/users",
             type: UsersReponse.self
         ) { [weak self] res in
-           
+
             DispatchQueue.main.async {
                 switch res {
                 case .success(let resultData):
--- Twaddle/TwaddleApp.swift
+++ Twaddle/TwaddleApp.swift
@@ -24,7 +24,6 @@ struct TwaddleApp: App {
                     .tabItem {
                         Symbols.person
                         Text("Home")
-                        Text("Temporary text for commit.")
                     }
             }
         }
