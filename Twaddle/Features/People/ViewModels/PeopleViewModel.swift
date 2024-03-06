f277b7331d40364be6e8907de22535ce94db3e0b
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-06 17:54:39 +0600

    Checkpoint day commit.

--- Twaddle/Features/People/ViewModels/PeopleViewModel.swift
+++ Twaddle/Features/People/ViewModels/PeopleViewModel.swift
@@ -14,7 +14,7 @@ final class PeopleViewModel: ObservableObject {
 
     func fetchUsers() {
         NetworkingManager.shared.request(
-            "https://reqres.in/api/usersx",
+            "https://reqres.in/api/users",
             type: UsersReponse.self
         ) { [weak self] res in
 
