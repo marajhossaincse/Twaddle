de6ac2b5bc1b4fcacb91dcf8bad773709e342c5a
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-17 15:47:48 +0600

    Add errors for form validation.

--- Twaddle/Features/Create/Validator/CreateValidator.swift
+++ Twaddle/Features/Create/Validator/CreateValidator.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct CreateValidator {
+    func validate(_ person: NewPerson) throws {}
+}
+
+extension CreateValidator {
+    enum CreateValidatorError: LocalizedError {
+        case invalidFirstName
+        case invalidLastName
+        case invalidJob
+    }
+}
+
+extension CreateValidator.CreateValidatorError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidFirstName:
+            return "First name can't be empty."
+        case .invalidLastName:
+            return "Last name can't be empty."
+        case .invalidJob:
+            return "Job can't be empty."
+        }
+    }
+}
