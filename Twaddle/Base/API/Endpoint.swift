952fe13cafaa08d4f5a33f38016d7d92d8729958
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-21 12:46:57 +0600

    Add endpoints.

---
+++ Twaddle/Base/API/Endpoint.swift
@@ -0,0 +1,14 @@
+//
+//  Endpoint.swift
+//  Twaddle
+//
+//  Created by Maraj Hossain on 21/3/24.
+//
+
+import Foundation
+
+enum Endpoint {
+    case people
+    case detail(id: Int)
+    case create
+}
