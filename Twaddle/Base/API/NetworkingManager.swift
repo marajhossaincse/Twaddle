b76cd4fbf551f5e7e1cd496ed19cacccec942d21
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-24 17:05:27 +0600

    Refactor fetch request with async.

--- Twaddle/Base/API/NetworkingManager.swift
+++ Twaddle/Base/API/NetworkingManager.swift
@@ -12,49 +12,32 @@ final class NetworkingManager {
 
     private init() {}
 
-    func request<T: Codable>(
-        _ endPoint: Endpoint,
-        type: T.Type) async
-    {
+    func request<T: Codable>(_ endPoint: Endpoint, type: T.Type) async throws -> T {
+        // Check to see if endpoint URL is valid else throw error
         guard let url = endPoint.url else {
-            completion(.failure(NetworkingError.invalidUrl))
-            return
+            throw NetworkingError.invalidUrl
         }
 
+        // Build the request
         let request = buildRequest(from: url, methodType: endPoint.methodType)
 
-        let dataTasks = URLSession.shared.dataTask(with: request) { data, response, error in
+        // Try to execute a fetch request and await for a value/data
+        let (data, response) = try await URLSession.shared.data(for: request)
 
-            if error != nil {
-                completion(.failure(NetworkingError.custom(error: error!)))
-
-                return
-            }
-
-            guard let response = response as? HTTPURLResponse,
-                  (200 ... 300) ~= response.statusCode
-            else {
-                let statusCode = (response as! HTTPURLResponse).statusCode
-                completion(.failure(NetworkingError.invalidStatusCode(statusCode: statusCode)))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(NetworkingError.invalidData))
-                return
-            }
-
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let result = try decoder.decode(T.self, from: data)
-
-                completion(.success(result))
-            } catch {
-                completion(.failure(NetworkingError.failedToDecode(error: error)))
-            }
+        // See if it receives a valid status code else throw error
+        guard let response = response as? HTTPURLResponse,
+              (200 ... 300) ~= response.statusCode
+        else {
+            let statusCode = (response as! HTTPURLResponse).statusCode
+            throw NetworkingError.invalidStatusCode(statusCode: statusCode)
         }
-        dataTasks.resume()
+
+        // Try decoding data using json decoder
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let result = try decoder.decode(T.self, from: data)
+
+        return result
     }
 
     func request(
