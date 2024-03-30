103a1fdb799023ef3e449930902fd3069f2bd14b
Maraj Hossain <marajhossaincse@gmail.com>
2024-03-30 18:49:58 +0600

    Refactor code.

--- Twaddle/TwaddleApp.swift
+++ Twaddle/TwaddleApp.swift
@@ -5,14 +5,6 @@
 //  Created by Maraj Hossain on 7/11/23.
 //
 
-/*
- Future Works:
-    - redesign home page
-    - perfect toolbar buttons
-    - add CRUD functions for users
-    - ...
- */
-
 import SwiftUI
 
 @main
