2fabd7a1bdfba345a0a106d9d79ad81e5cc276ac
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-04 19:39:15 +0600

    Add possible test case functions.

--- TwaddleTests/Mapper/JSONMapperTests.swift
+++ TwaddleTests/Mapper/JSONMapperTests.swift
@@ -8,6 +8,10 @@
 import Foundation
 import XCTest
 
-class JSONMapperTests: XCTestCase{
+class JSONMapperTests: XCTestCase {
+    func test_with_valid_json_successfully_decoded() {}
     
+    func test_with_missing_file_error_thrown() {}
+    
+    func test_with_invalid_json_error_thrown() {}
 }
