bc9d858029fdac246467d97074a2e107858b8a4e
Maraj Hossain <marajhossaincse@gmail.com>
2024-04-05 13:06:09 +0600

    WIP: Add unit test for json mapper and code refactor.

--- Twaddle/Base/Mapper/StaticJSONMapper.swift
+++ Twaddle/Base/Mapper/StaticJSONMapper.swift
@@ -8,20 +8,21 @@
 import Foundation
 
 struct StaticJSONMapper {
-    static func decode<T: Decodable>(file: String, type: T.Type) throws -> T{
+    static func decode<T: Decodable>(file: String, type: T.Type) throws -> T {
         guard let path = Bundle.main.path(forResource: file, ofType: "json"),
-        let data = FileManager.default.contents(atPath: path) else {
+              let data = FileManager.default.contents(atPath: path)
+        else {
             throw MappingError.failedToGetContents
         }
-        
+
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
         return try decoder.decode(T.self, from: data)
     }
 }
 
-extension StaticJSONMapper{
-    enum MappingError: Error{
+extension StaticJSONMapper {
+    enum MappingError: Error {
         case failedToGetContents
     }
 }
--- TwaddleTests/Mapper/JSONMapperTests.swift
+++ TwaddleTests/Mapper/JSONMapperTests.swift
@@ -6,12 +6,19 @@
 //
 
 import Foundation
+@testable import Twaddle
 import XCTest
 
 class JSONMapperTests: XCTestCase {
-    func test_with_valid_json_successfully_decoded() {}
-    
-    func test_with_missing_file_error_thrown() {}
-    
-    func test_with_invalid_json_error_thrown() {}
+    func test_with_valid_json_successfully_decoded() {
+        XCTAssertNoThrow(try StaticJSONMapper.decode(file: "UsersStaticData", type: UsersReponse.self), "Mapper shouldn't throw an error.")
+    }
+
+    func test_with_missing_file_error_thrown() {
+        XCTFail()
+    }
+
+    func test_with_invalid_json_error_thrown() {
+        XCTFail()
+    }
 }
